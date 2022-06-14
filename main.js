f72638524a0aacd7626a55c6556f6da22f66a1dc
sleepingontheriver <88553747+sleepingontheriver@users.noreply.github.com>
2022-06-14 13:10:33 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+const character = document.getElementById("character")
+
+let PlayerSquare  = new Object
+
+PlayerSquare.x = 0
+PlayerSquare.y = 0
+PlayerSquare.dx = 0
+PlayerSquare.dy = 0
+PlayerSquare.ddx = 0
+PlayerSquare.ddy = 0
+
+function Update_Pos(x, y){
+    PlayerSquare.x = PlayerSquare.x + x
+    PlayerSquare.y = PlayerSquare.y + y
+}
+
+function Draw_Player(){
+    Update_Pos(PlayerSquare.dx, PlayerSquare.dy)
+    character.style.top = PlayerSquare.y + "px"
+    character.style.left = PlayerSquare.x + "px"
+};
+
+function Update_Size(height, width){
+    character.style.height = height + "px"
+    character.style.width = width + "px"
+};
+
+character.addEventListener("click", () => {
+    console.log("Button clicked");
+    Update_Pos(Math.random() * 100, Math.random() * 100)
+});
+
+addEventListener('keydown', ({key}) => {
+    switch (key){
+        case 'a':
+            console.log("left")
+            PlayerSquare.dx = -2.8
+            PlayerSquare.dy = 0
+            break
+        case 'd':
+            console.log("right")
+            PlayerSquare.dx = 2.8
+            PlayerSquare.dy = 0
+            break
+        case 'w':
+            console.log("up")
+            PlayerSquare.dy = -2.8
+            PlayerSquare.dx = 0
+            break
+        case 's':
+            console.log("down")
+            PlayerSquare.dy = 2.8
+            PlayerSquare.dx = 0
+            break
+            
+        
+    }
+})
+
+
+
+
+
+function update(progress) {
+    // Update the state of the world for the elapsed time since last render
+  }
+  
+  function draw() {
+    Draw_Player()
+  }
+  
+function loop(timestamp) {
+    var progress = timestamp - lastRender
+  
+    update(progress)
+    draw()
+  
+    lastRender = timestamp
+    window.requestAnimationFrame(loop)
+}
+  var lastRender = 0
+  window.requestAnimationFrame(loop)
+  
